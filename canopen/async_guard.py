aa65290ae303aebbdb035da9737990cebfff5d06
Jonas Tittmann <j.tittmann@pinion.eu>
2024-04-29 15:42:03 +0200

    Add Traceback to identify caller of blocking function while running async

--- canopen/async_guard.py
+++ canopen/async_guard.py
@@ -1,7 +1,7 @@
 """ Utils for async """
 
-import functools
-from typing import Optional, Callable
+import traceback
+from typing import Optional, Callab
 
 TSentinel = Callable[[], bool]
 
@@ -26,6 +26,9 @@ def ensure_not_async(fn):
         global _ASYNC_SENTINEL
         if _ASYNC_SENTINEL:
             if _ASYNC_SENTINEL():
+                print("Traceback (most recent call last):")
+                for line in traceback.format_stack():
+                    print(line.strip())
                 raise RuntimeError("Calling a blocking function while running async")
         return fn(*args, **kwargs)
     return async_guard
